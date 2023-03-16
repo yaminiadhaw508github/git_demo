1f9cb64bc2ff1e051f7fa57e89666fe210a3f873
Yamini <yaminiadhaw508@gmail.com>
2023-03-17 00:24:22 +0530

    base project

---
+++ src/com/demo/test.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {10,20,30,40};
+		for(int i:arr)
+			System.out.println(i);
+
+	}
+
+}
